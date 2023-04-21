e07769e48d45350b4c0683def60b7124cf85720d
ErikCald <erikcaell@gmail.com>
2023-04-21 17:14:05 -0400

    Reformatted template code

---
+++ src/main/java/frc/robot/Config.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Config class provides a convenient place for robot-wide numerical or
+ * boolean constants. This class should not be used for any other purpose. All
+ * constants should be declared globally (i.e. public static). Do not put
+ * anything functional in this class.
+ */
+public final class Config {
+    public static class JoystickConfig {
+        public static final int DRIVER_JOYSTICK_PORT = 0;
+        public static final int OPERATOR_JOYSTICK_PORT = 1;
+    }
+}
