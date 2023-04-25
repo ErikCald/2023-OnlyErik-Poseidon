e9524c530bc56d33e3ef53e3141b6d8a25997d2f
ErikCald <erikcaell@gmail.com>
2023-04-24 20:30:13 -0400

    Created ArmPaths to use PathPlanner arm stuff

---
+++ src/main/java/frc/robot/subsystems/ArmPaths.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.custompathplanner.ArmAutoBuilder;
+import frc.robot.Config.Arm.ArmPathPlanner;
+
+/** Add your docs here. */
+public class ArmPaths {
+    ArmAutoBuilder armBuilder;
+    ArmSubsystem armSub;
+
+    PathPlannerTrajectory pickupToTop,
+                          topToMiddle,
+                          middleToPickup;
+
+
+    public ArmPaths(ArmSubsystem armSubsystem) {
+        armSub = armSubsystem;
+        Map<String, Command> eventMap = new HashMap<String, Command>();
+
+        armBuilder = new ArmAutoBuilder(
+            armSub::forwardKinematicsPose,
+            ArmPathPlanner.TRANSLATION_PID_CONSTANTS, 
+            ArmPathPlanner.ROTATION_PID_CONSTANTS, 
+            armSub::setArmSpeeds, 
+            eventMap, 
+            armSub
+        );
+
+        buildPaths();
+    }
+
+    private void buildPaths() {
+        pickupToTop = PathPlanner.loadPath("homeToTop", ArmPathPlanner.VEL, ArmPathPlanner.ACCEL);
+        topToMiddle = PathPlanner.loadPath("topToMiddle", ArmPathPlanner.VEL, ArmPathPlanner.ACCEL);
+        middleToPickup = PathPlanner.loadPath("middleToHome", ArmPathPlanner.VEL, ArmPathPlanner.ACCEL);
+    }
+
+    public CommandBase pickupToTopCommand() {
+        return armBuilder.followPath(pickupToTop);
+    }
+
+    public CommandBase topToMiddleCommand() {
+        return armBuilder.followPath(topToMiddle);
+    }
+
+    public CommandBase middleToPickupCommand() {
+        return armBuilder.followPath(middleToPickup);
+    }
+
+
+
+}
